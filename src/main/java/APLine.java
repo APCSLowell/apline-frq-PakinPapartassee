38d02e5a215538f2d2c6c69d04838597720938e6
luigimangione#1fan <pakininninin@gmail.com>
2025-04-24 21:56:18 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,18 +1,15 @@
 public class APLine
 {
   private int aNum,bNum,cNum;
-  private double slope;
   public APLine(int a, int b, int c){
     aNum =a;
     bNum = b;
     cNum = c;
-    slope = -(double)aNum/bNum;
   }
   public double getSlope(){
-    return slope;
+    return -(double)a/b;
   }
   public boolean isOnline(double x, double y){
-    if(((aNum*x)+(bNum*y)+cNum)==0){return true;}
-    return false;
+    return aNum*x + bNum*y + cNum ==0;
   }
 }
